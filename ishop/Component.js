8b0cdd8ac4613846f2b29149f64f18fb9b1bbcf1
alexsky1991 <lehakononov199@gmail.com>
2019-03-17 16:21:50 +0300

    'commit'

---
+++ ishop/Component.js
@@ -0,0 +1,46 @@
+var Component = React.createClass({
+
+    displayName: 'Component',
+
+    propTypes: {
+        name: React.PropTypes.string.isRequired,
+        list:React.PropTypes.arrayOf(
+            React.PropTypes.shape({
+              name: React.PropTypes.string.isRequired,
+              price: React.PropTypes.number.isRequired,
+              number: React.PropTypes.number.isRequired,
+              url: React.PropTypes.string.isRequired
+            })
+          )
+    },
+
+    render: function(){
+        var {list, name} = this.props;
+        console.log(name);
+
+        var table = list.map(e => {
+            
+            return  React.DOM.div({key: e.name, className: 'shop_str'}, 
+                        React.DOM.span(null, e.name),
+                        React.DOM.span(null, e.price),
+                        React.DOM.span(null, e.number),
+                        React.DOM.span(null, e.url),
+                )
+            
+        });
+
+        var panel = React.DOM.div({className: 'shop_str'}, 
+            React.DOM.span(null , 'Имя'),
+            React.DOM.span(null , 'Цена'),
+            React.DOM.span(null , 'Количество'),
+            React.DOM.span(null , 'Картинка'),
+        )
+
+        var shop = React.DOM.div({className: 'shop'}, panel, table)
+
+        return React.DOM.div(null,
+            React.DOM.h1(null, `Магазин ${name}`), shop
+        )
+    
+    },
+})
