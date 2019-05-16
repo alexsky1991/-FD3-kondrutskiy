5c76ce583926a100b616d7b397989082cee1a4ec
a.kondrutskiy <a.kondrutskiy@sw-tech.by>
2019-05-16 10:14:25 +0300

    modify test

--- Mobile2/__tests__/removeClient-test.js
+++ Mobile2/__tests__/removeClient-test.js
@@ -6,6 +6,6 @@ const arr = [{id:1},{id:2}]
 
 test('проверка удаления клиента', () => {
 
-  expect(removeClient(1,[{id:1},{id:2}])).toBe([{id:2}]);
+  expect(removeClient(1,[{id:1},{id:2}])).toEqual([{id:2}]);
 
 });
