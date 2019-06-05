a3b497f73a7efb84d3aee8d32bde5e9819479fe7
a.kondrutskiy <a.kondrutskiy@sw-tech.by>
2019-06-05 11:59:01 +0300

    add scales2

---
+++ Scales2/app.js
@@ -0,0 +1,56 @@
+var Scales = /** @class */ (function () {
+    function Scales() {
+        var _this = this;
+        this.products = [];
+        this.add = function (item) {
+            _this.products.push(item);
+        };
+        this.getSumScale = function () {
+            var result = _this.products.reduce(function (a, b) { return a + b.weight; }, 0);
+            return result;
+        };
+        this.getNameList = function () {
+            var result = _this.products.map(function (el) { return el.name; });
+            return result;
+        };
+    }
+    return Scales;
+}());
+var Apple = /** @class */ (function () {
+    function Apple(name, weight) {
+        var _this = this;
+        this.getScale = function () {
+            console.log(_this.weight);
+        };
+        this.getName = function () {
+            console.log(_this.name);
+        };
+        this.name = name;
+        this.weight = weight;
+    }
+    return Apple;
+}());
+var Tomato = /** @class */ (function () {
+    function Tomato(name, weight) {
+        var _this = this;
+        this.getScale = function () {
+            console.log(_this.weight);
+        };
+        this.getName = function () {
+            console.log(_this.name);
+        };
+        this.name = name;
+        this.weight = weight;
+    }
+    return Tomato;
+}());
+var apple1 = new Apple('яблоко', 52);
+var apple2 = new Apple('помидор1', 32);
+var tomato1 = new Tomato('помидор2', 15);
+var scales1 = new Scales();
+scales1.add(apple1);
+scales1.add(apple2);
+scales1.add(tomato1);
+console.log(scales1.getSumScale());
+console.log(scales1.getNameList());
+//# sourceMappingURL=app.js.map
---
+++ Scales2/app.ts
@@ -0,0 +1,85 @@
+
+interface IScalable {
+
+    getScale():void;
+
+    getName():void;
+}
+
+
+class Scales {
+
+    products:any[] = [];
+
+    add = (item:IScalable):void => {
+        this.products.push(item);
+    }
+
+    getSumScale = ():number => {
+        let result:number = this.products.reduce((a,b) => a + b.weight, 0);
+        return result;
+    }
+
+    getNameList = ():string[] => {
+        let result:string[] = this.products.map(el => el.name)
+        return result;
+    }
+}
+
+
+class Apple implements IScalable {
+
+    name:string;
+    weight:number;
+
+    constructor(name:string, weight:number){
+        this.name = name;
+        this.weight = weight;
+    }
+
+    getScale = ():void => {
+        console.log(this.weight)
+    }
+
+    getName = ():void => {
+        console.log(this.name)
+    }
+}
+
+class Tomato implements IScalable  {
+
+    name:string;
+    weight:number;
+
+    constructor(name:string, weight:number){
+        this.name = name;
+        this.weight = weight;
+    }
+
+    getScale = ():void => {
+        console.log(this.weight)
+    }
+
+    getName = ():void => {
+        console.log(this.name)
+    }
+}
+
+
+let apple1:Apple = new Apple('яблоко', 52);
+let apple2:Apple = new Apple('помидор1', 32);
+let tomato1:Tomato = new Tomato('помидор2', 15);
+
+let scales1:Scales = new Scales();
+
+scales1.add(apple1);
+scales1.add(apple2);
+scales1.add(tomato1);
+
+console.log(scales1.getSumScale());
+console.log(scales1.getNameList());
+
+
+
+
+
