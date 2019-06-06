f8d601fb9e18eee3214133f7bd0b376ef1853cbf
a.kondrutskiy <a.kondrutskiy@sw-tech.by>
2019-06-06 10:39:24 +0300

    add localStorage

--- Scales3/app.js
+++ Scales3/app.js
@@ -1,11 +1,24 @@
 var Scales = /** @class */ (function () {
-    function Scales(obj) {
+    function Scales(item) {
         var _this = this;
-        this.getSumScale = function () {
-            console.log(_this.item);
-            return 1;
+        this.item = item;
+        this.add = function (_item) {
+            _this.item.addItem(_item);
         };
-        this.item = obj;
+        this.getSumScale = function () {
+            var _sumScale = 0;
+            for (var i = 0; i < _this.item.getCount(); i++) {
+                _sumScale += _this.item.getItem(i).getScale();
+            }
+            return _sumScale;
+        };
+        this.getNameList = function () {
+            var _nameList = [];
+            for (var i = 0; i < _this.item.getCount(); i++) {
+                _nameList.push(_this.item.getItem(i).getName());
+            }
+            return _nameList;
+        };
     }
     return Scales;
 }());
@@ -16,34 +29,46 @@ var ScalesStorageEngineArray = /** @class */ (function () {
         this.addItem = function (item) {
             _this.products.push(item);
         };
-        this.getItem = function (index) {
-            return _this.products[index];
-        };
-        this.getCount = function () {
-            return _this.products.length;
-        };
+        this.getItem = function (index) { return _this.products[index]; };
+        this.getCount = function () { return _this.products.length; };
     }
     return ScalesStorageEngineArray;
+}());
+var ScalesStorageEngineLocalStorage = /** @class */ (function () {
+    function ScalesStorageEngineLocalStorage() {
+        var _this = this;
+        this.number = 0;
+        this.addItem = function (item) {
+            var json = JSON.stringify(item);
+            localStorage.setItem(_this.number.toString(), json);
+            _this.number++;
+        };
+        this.getItem = function (index) {
+            var res = JSON.parse(localStorage[index]);
+            return res;
+        };
+        this.getCount = function () { return _this.number; };
+    }
+    return ScalesStorageEngineLocalStorage;
 }());
 var Product = /** @class */ (function () {
     function Product(name, weight) {
         var _this = this;
-        this.getScale = function () {
-            console.log(_this.weight);
-        };
-        this.getName = function () {
-            console.log(_this.name);
-        };
         this.name = name;
         this.weight = weight;
+        this.getScale = function () { return _this.weight; };
+        this.getName = function () { return _this.name; };
     }
     return Product;
 }());
 var apple1 = new Product('яблоко', 52);
 var apple2 = new Product('помидор1', 32);
 var tomato1 = new Product('помидор2', 15);
-var scales1 = new ScalesStorageEngineArray();
-scales1.addItem(apple1);
-scales1.addItem(apple2);
-scales1.addItem(tomato1);
+var arr = new ScalesStorageEngineLocalStorage();
+var scales1 = new Scales(arr);
+scales1.add(apple1);
+scales1.add(apple2);
+scales1.add(tomato1);
+console.log(scales1.getSumScale());
+console.log(scales1.getNameList());
 //# sourceMappingURL=app.js.map
--- Scales3/app.ts
+++ Scales3/app.ts
@@ -3,7 +3,7 @@ interface IStorageEngine{
 
     addItem(item:any):void;
 
-    getItem(index:number):void;
+    getItem(index:number):Product;
 
     getCount():number;
 }
@@ -11,19 +11,31 @@ interface IStorageEngine{
 
 class Scales<StorageEngine extends IStorageEngine> {
 
-
     constructor(public item:StorageEngine){}
 
-
-    getSumScale = ():number => {
-        let result:number = this.item.products.reduce((a,b) => a + b.weight, 0);
-        return result;
+    add = (_item:Product):void => {
+        this.item.addItem(_item)
     }
 
-    // getNameList = ():string[] => {
-    //     let result:string[] = this.products.map(el => el.name)
-    //     return result;
-    // }
+    getSumScale = ():number => {
+       let _sumScale:number = 0;
+
+        for (let i = 0; i<this.item.getCount(); i++) {
+            _sumScale += this.item.getItem(i).getScale();
+        }
+
+       return _sumScale
+    }
+
+    getNameList = ():string[] => {
+        let _nameList:string[] = [];
+
+        for (var i = 0; i < this.item.getCount(); i++) {
+           _nameList.push(this.item.getItem(i).getName())
+        }
+
+        return _nameList;
+    }
 }
 
 
@@ -35,34 +47,40 @@ class ScalesStorageEngineArray implements IStorageEngine{
         this.products.push(item);
     }
 
+    getItem = (index:number):Product => this.products[index];
+    
+    getCount = ():number => this.products.length;
+     
+}
+
+class ScalesStorageEngineLocalStorage implements IStorageEngine{
+
+    number:number = 0;
+
+    addItem = (item:Product):void => {
+        var json = JSON.stringify(item)
+
+        localStorage.setItem(this.number.toString(), json)
+        this.number++;
+    }
+       
     getItem = (index:number):Product => {
-        return this.products[index];
+       let res:Product = JSON.parse(localStorage[index])
+       return res
     }
 
-    getCount = ():number => {
-        return this.products.length
-    }
-    
+    getCount = ():number => this.number; 
+
 }
 
 class Product{
 
-    name:string;
-    weight:number;
+    constructor(public name:string, public weight:number) {}
 
-    constructor(name:string, weight:number) {
-        this.name = name;
-        this.weight = weight;
-    }
-
-    getScale = ():void => {
-        console.log(this.weight)
-    }
-
-    getName = ():void => {
-        console.log(this.name)
-    }
-
+    getScale = ():number => this.weight;
+    
+    getName = ():string => this.name;
+   
 }
 
 
@@ -70,17 +88,17 @@ let apple1:Product = new Product('яблоко', 52);
 let apple2:Product = new Product('помидор1', 32);
 let tomato1:Product = new Product('помидор2', 15);
 
-let scales = new ScalesStorageEngineArray();
+let arr = new ScalesStorageEngineLocalStorage();
 
-let scales1 = new Scales<ScalesStorageEngineArray>(scales);
+let scales1 = new Scales<ScalesStorageEngineLocalStorage>(arr);
 
-// scales1.addItem(apple1);
-// scales1.addItem(apple2);
-// scales1.addItem(tomato1);
+scales1.add(apple1);
+scales1.add(apple2);
+scales1.add(tomato1);
 
 
 console.log(scales1.getSumScale());
-// console.log(scales1.getNameList());
+console.log(scales1.getNameList());
 
 
 
